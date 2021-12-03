ac76fddb23f5c6e580ecaf0fdba316e3cd305f39
Nathan Nancy <nathan.nancy@icloud.com>
2021-12-03 11:18:39 +0100

    add tensorflow

--- streamlit/page3.py
+++ streamlit/page3.py
@@ -3,7 +3,6 @@ import streamlit.components.v1 as components
 
 def app():
     visualisation = 'https://projector.tensorflow.org/?config=https://gist.githubusercontent.com/Yanka13/ab939e88821f32da1ad5fc32149b6261/raw/0c5f257bbeb035a4d7708b49cab447044dd7911b/player2vec_config.txt'
+    st.write(f"check out this [link]({visualisation})")
+
     components.iframe("https://projector.tensorflow.org/", scrolling=False, width=1200, height=800)
-    st.write(
-        f"check out this [link]({visualisation})"
-    )
