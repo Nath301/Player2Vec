b08d0572a7ba18cef4876c4c48ed396974c8cf03
Nathan Nancy <nathan.nancy@icloud.com>
2021-12-03 13:28:31 +0100

    remove action2vec

--- streamlit/page2.py
+++ streamlit/page2.py
@@ -4,19 +4,6 @@ import pickle
 
 st.markdown('<style>h5{color: #659DBD}</style>', unsafe_allow_html=True)
 
-
-def plot_action():
-    st.write('##### Vizualisation Action2Vec')
-    file = open('pickle/action2vec.pickle', 'rb')
-    fig = pickle.load(file)
-    fig.update_layout(
-        plot_bgcolor='#C5C6C7',
-        autosize=False,
-        width=800,
-        height=600,
-    )
-    st.plotly_chart(fig)
-
 def plot_player():
     st.write('##### Vizualisation Player2Vec')
     file = open('pickle/umap_player_model.pickle', 'rb')
@@ -43,5 +30,4 @@ def plot_3d():
 
 def app():
     plot_player()
-    plot_action()
     plot_3d()
